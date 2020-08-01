84b2891eeb1f06950bc9783e92278829408e0223
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-01 23:36:24 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,28 +5,32 @@ $countryCode = $_POST['countryCode'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 $gender = $_POST['gender'];
-$Languages = $_POST['Languages'];
+$checkbox1=$_POST['language']; 
 $msg = $_POST['msg'];
 
+ if (!empty($fname) || !empty($lname) || !empty($countryCode) || !empty($phone) || !empty($email) || !empty($gender) || !empty($Languages) || !empty($msg))
+{ $host = "localhost";
+  $dbusername = "root";
+  $dbpassword = "root";
+  $dbname = "volunteer";
+  $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
+  if(isset($_POST['submit']))  
+{  
+$conn=mysqli_connect("$host", "$username", "$word","$db_name")or die("cannot connect");//connection string 
+ 
+$chk="";  
+foreach($checkbox1 as $chk1)  
+   {  
+      $chk .= $chk1.",";  
+   }
+   $in_ch=mysqli_query($conn,"insert into request_quote(language) values ('$chk')");  
+   
+}  
 
 
-
-
-
-
-
-
-if (!empty($fname) || !empty($lname) || !empty($countryCode) || !empty($phone) || !empty($email) || !empty($gender) || !empty($Languages) || !empty($msg))
-{ $host = 'healthmatch-server.mysql.database.azure.com';
-  $username = 'HEALTHMATCH@healthmatch-server';
-  $password = 'Hackathon2020';
-  $db_name = 'volunteer';
-  
-  //Establishes the connection
-  $conn = mysqli_init();
-  mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-  if (mysqli_connect_errno($conn)) {
-  die('Failed to connect to MySQL: '.mysqli_connect_error());
+  if (mysqli_connect_error())
+  {
+    die('Connect Error('.mysqli_connect_errorno().')'. mysqli_connect_error());
   }
   else {
     $SELECT = "SELECT email From resister Where email =? Limit 1 ";
@@ -39,11 +43,9 @@ if (!empty($fname) || !empty($lname) || !empty($countryCode) || !empty($phone) |
     $rnum= $stmt->num_rows;
     if($rnum==0){$stmt->close();
     $stmt = $conn->prepare($INSERT);
-    $b=implode(",",$Languages);
-    $stmt->bind_param("ssiissss",$fname,$lname,$countryCode,$phone,$email,$gender,$b,$msg);
+    $stmt->bind_param("ssiissss",$fname,$lname,$countryCode,$phone,$email,$gender,$Languages,$msg);
     $stmt->execute();
-    header("location: thankyouvolunteer.php");
-    exit;
+   require 'thankyouvolunteer.php';
   }
   else{ echo"there exists";}
   $stmt->close();
@@ -56,3 +58,5 @@ if (!empty($fname) || !empty($lname) || !empty($countryCode) || !empty($phone) |
     die();
   }
   ?>
+
+ 

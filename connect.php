3761e0062668a81a3f91218fcf5a84468581bc93
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-08 14:27:57 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="signupvolunteer.css" />
+    <script
+      type="text/javascript"
+      src="https://cdn.weglot.com/weglot.min.js"
+	></script>
+
+	 <script>
+        Weglot.initialize({
+          api_key: "wg_6595b397394b082513ae5269c8992a3d7",
+        });
+      </script>
+    <style>
+
+
+@import url("https://fonts.googleapis.com/css?family=Abel|Abril+Fatface|Alegreya|Arima+Madurai|Dancing+Script|Dosis|Merriweather|Oleo+Script|Overlock|PT+Serif|Pacifico|Playball|Playfair+Display|Share|Unica+One|Vibur");
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-image: linear-gradient(-225deg, #e3fdf5 0%, #ffe6fa 100%);
+  background-image: linear-gradient(to top, #6be7e1 0%, #fed6e3 100%);
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+
+  font-family: "Vibur", cursive;
+  /*   the main font */
+  font-family: "Abel", sans-serif;
+  opacity: 0.95;
+  /* background-image: linear-gradient(to top, #d9afd9 0%, #97d9e1 100%); */
+}
+
+
+
+
+select {
+  background-color: rgba(0, 0, 0, 0.644);
+  width: 30%;
+  margin-right: 0;
+  height: 50px;
+  border-radius: 5px 5px 5px 5px;
+  color: white;
+  margin-top: 2%;
+  padding: 15px;
+  font-size: 16px;
+  font-family: "Abel", sans-serif;
+}
+
+::placeholder {
+
+  color: white;
+}
+
+      .volunteerlogo {
+        width: 40%;
+        text-align: center;
+      }
+     
+      }
+    </style>
+  </head>
+  <body>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	  <br>
+    <div class="overlay">
+    <div style="text-align: center;">  <img
+        src="logo.png"
+        class="volunteerlogo"
+        style="padding-left: 2rem;"
+      /></div>
 <?php
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
@@ -57,3 +134,52 @@ if (!empty($fname) || !empty($lname) || !empty($countryCode) || !empty($phone) |
     die();
   }
   ?>
+
+  <form action=""  method="post" name="google-sheet" id="HideBlock">
+
+      <input type="text" class="form-control"
+    name = "FirstName" value = "<?php echo $_POST['fname'];?>" readonly>
+    <br>
+     <input type="text" class="form-control"
+    name = "LastName" value = "<?php echo $_POST['lname'];?>" readonly>
+    <br>
+     <input type="email" class="form-control"
+    name = "Email" value = "<?php echo $_POST['email'];?>" readonly>
+    
+    <br>
+    
+
+<div style="text-align:center">
+<div style="text-align: center;"> <button class ="acceptbtn":hover name="submit"  id="showHiddenBlock" >Proceed
+
+
+      </button></div>
+      </form>
+<div style="text-align: center;" id="initiallyHiddenBlock" class="margin">
+<br>
+      <h1>Submitted!</h1>
+</div>
+
+ <script
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"
+      ></script>
+
+
+     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+     <script>
+     $(document).ready(function() {
+      $('#showHiddenBlock').click(function() {
+          $('#initiallyHiddenBlock').show();
+		  $('#HideBlock').hide();
+      });
+  });
+  </script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </body>
+</html>
